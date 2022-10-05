4048bd2bab961b2d068744ca17500afa0ce3c1b6
Dave Tan <davetankx@gmail.com>
2022-10-05 17:58:09 +0800

    added font, logo and relevant styles finished the header on start page

--- FaultTracker/src/View/Main.java
+++ FaultTracker/src/View/Main.java
@@ -7,20 +7,24 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage){
         try {
             Stage mainStage = new Stage();
-            Parent root = FXMLLoader.load(getClass().getResource("AboutTab.fxml"));
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Start.fxml")));
+
             mainStage.setTitle("Fault");
 //            mainStage.getIcons().add(new Image("file:" +  System.getProperty("user.dir") + "/resources/GroSureMini.png"));
-            mainStage.setScene(new Scene(root, 335, 600));
+            mainStage.setScene(new Scene(root, 1080, 680));
             mainStage.show();
+            mainStage.setResizable(false);
         }
         catch (Exception ex)
         {
