26be24f35d3135ce1be22ba1ad71a275ce60b2e1
notmeegoreng <notmeegoreng@gmail.com>
2022-10-06 10:45:55 +0800

    add parent to rbnode
    finish init
    add blackHeight
    start on insert function

--- FaultTracker/src/Model/RBNode.java
+++ FaultTracker/src/Model/RBNode.java
@@ -5,21 +5,27 @@ public class RBNode<T> {
     boolean black;
     RBNode<T> left;
     RBNode<T> right;
+    RBNode<T> parent;
 
     public RBNode(T item, boolean black){
         this.item = item;
         this.black = black;
     }
 
-    public RBNode(T item, boolean black, RBNode<T> left, RBNode<T> right){
+    public RBNode(T item, boolean black, RBNode<T> parent) {
         this(item, black);
+        this.parent = parent;
+    }
+
+    public RBNode(T item, boolean black, RBNode<T> parent, RBNode<T> left, RBNode<T> right){
+        this(item, black, parent);
         this.left = left;
         this.right = right;
     }
 
     // Copy constructor to copy the Node object n
     public RBNode(RBNode<T> n){
-        this(n.item, n.black, n.left, n.right);
+        this(n.item, n.black, n.parent, n.left, n.right);
     }
 
     // Accessor method returns item stored in Node
--- FaultTracker/src/Model/RBTree.java
+++ FaultTracker/src/Model/RBTree.java
@@ -6,8 +6,14 @@ public class RBTree<T extends Comparable<? super T>> {
         root = null;
     }
 
-    public RBTree(T[] items) {
+    public RBTree(T item) {
+        root = new RBNode<>(item, true);
+    }
 
+    public RBTree(T[] items) {
+        for (T item : items) {
+            insert(item);
+        }
     }
 
     private RBNode<T> rightRotate(RBNode<T> node){
@@ -25,4 +31,43 @@ public class RBTree<T extends Comparable<? super T>> {
         node.right = right_left;
         return null;
     }
+
+    public int blackHeight() {
+        RBNode<T> curr = root;
+        int h = 2;
+        while (curr.left != null) {
+            curr = curr.left;
+            if (curr.black) h++;
+        }
+        return h;
+    }
+
+    public void insert(T item) {
+        if (root == null) {
+            root = new RBNode<>(item, true);
+            return;
+        }
+        insert(item, root);
+    }
+
+    private void insert(T item, RBNode<T> curr) {
+        // Complete the recursive code for insertion below this comment
+        int c = item.compareTo(curr.getItem());
+        if (c < 0) {
+            if (curr.left == null) {
+                curr.left = new RBNode<>(item, false, curr);
+                // todo: fix red-red
+                return;
+            }
+            insert(item, curr.left);
+        } else if (c == 0) {
+            throw new IllegalArgumentException(String.format("RB Tree already contains the value %s!", item));
+        } else if (curr.right == null) {
+            curr.right = new RBNode<>(item, false, curr);
+            // todo: fix red-red
+            return;
+        }
+        insert(item, curr.right);
+    }
+
 }
