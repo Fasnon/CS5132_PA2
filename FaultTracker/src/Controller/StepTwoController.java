8ec8d0731bccec9eb62981f49a18ea2580ad838d
Dave Tan <davetankx@gmail.com>
2022-10-11 14:16:26 +0800

    change tf to cb

--- FaultTracker/src/Controller/StepTwoController.java
+++ FaultTracker/src/Controller/StepTwoController.java
@@ -1,18 +1,22 @@
 package Controller;
 
 import Model.Report;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Objects;
 import java.util.ResourceBundle;
@@ -34,18 +38,26 @@ public class StepTwoController implements Initializable {
     private Button proceedBtn;
 
     @FXML
-    private TextField severityTF;
-
-    @FXML
     private TextField titleTF;
 
-    @FXML
-    private TextField urgencyTF;
 
     private String type;
 
+
+    @FXML
+    private ChoiceBox<Integer> severityCB;
+
+    @FXML
+    private ChoiceBox<Integer> urgencyCB;
+
+
     public void initData(String type){
         this.type = type;
+;
+        severityCB.setItems(FXCollections.observableArrayList(1,2,3));
+        severityCB.setValue(1);
+        urgencyCB.setItems(FXCollections.observableArrayList(1,2,3));
+        urgencyCB.setValue(1);
     }
 
     @FXML
@@ -64,7 +76,7 @@ public class StepTwoController implements Initializable {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/StepThree.fxml"));
         primaryStage.getScene().setRoot(loader.load());
 //        String type = (String) primaryStage.getUserData();
-        Report userReport = new Report(type, titleTF.getText(), descTA.getText(), contactTF.getText(), locTF.getText(), Integer.parseInt(urgencyTF.getText()), Integer.parseInt(severityTF.getText()), new Date());
+        Report userReport = new Report(type, titleTF.getText(), descTA.getText(), contactTF.getText(), locTF.getText(), urgencyCB.getSelectionModel().getSelectedItem(), severityCB.getSelectionModel().getSelectedItem(), new Date());
 //        primaryStage.setUserData(userReport);
         StepThreeController controller = loader.getController();
         controller.initData(userReport);
