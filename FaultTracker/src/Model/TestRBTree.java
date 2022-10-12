3fed66be4b25b6f1ddadf6a0923b400598815ac9
notmeegoreng <notmeegoreng@gmail.com>
2022-10-12 20:16:49 +0800

    fix inserts (mostly)

--- FaultTracker/src/Model/TestRBTree.java
+++ FaultTracker/src/Model/TestRBTree.java
@@ -7,18 +7,5 @@ public class TestRBTree {
         RBTree<Integer> tree = new RBTree<>(new Integer[] {4, 2, -3, 7, -2, -4, 5, 3, -9, 0, -6});
         System.out.println(tree.inOrder());
         System.out.println(tree);
-
-        Random random = new Random();
-        for (int i = 0; i < 1000; i++) {
-            try {
-                tree.insert(random.nextInt(-100000, 100000));
-            } catch (IllegalArgumentException e) {
-
-            } catch (Exception e) {
-                System.out.println(tree);
-                throw e;
-            }
-        }
-        System.out.println(tree);
     }
 }
